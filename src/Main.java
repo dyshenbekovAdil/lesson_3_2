a925b129641675d369abad4bc042440550c2d958
Aikeldi <aykeldi@inbox.ru>
2022-08-16 12:38:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] numbers = {-7.3, -9.2,-7.2, -9.6, -2.2, -1.9, 3.3, 92.3, 0.8, 511.7, 6.4,  0.4, 511.8, 6.7, 5.8};
+        for (int i = 0; i <numbers.length; i++) {
+            
+        }
+    }
+}
